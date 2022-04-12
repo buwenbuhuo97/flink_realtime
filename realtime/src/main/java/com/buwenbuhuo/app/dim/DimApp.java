4e67a979b0bc268571ebad58329fd5fad9033e13
buwenbuhuo <buwenbuhuo@lucky.com>
2022-04-12 20:39:30 +0800

    04月12日更新

---
+++ realtime/src/main/java/com/buwenbuhuo/app/dim/DimApp.java
@@ -0,0 +1,39 @@
+package com.buwenbuhuo.app.dim;
+
+/**
+ * Author 不温卜火
+ * Create 2022-04-12 20:36
+ * MyBlog https://buwenbuhuo.blog.csdn.net
+ * Description: Dim主程序入口
+ */
+public class DimApp {
+    public static void main(String[] args) {
+        // TODO 1.获取执行环境
+
+
+        // TODO 2.读取Kafka topic_db主题数据创建流
+
+
+        // TODO 3.过滤掉非JSON格式的数据，并将其写入侧输出流
+
+
+        // TODO 4.使用FlinkCDC读取MySQL中的配置信息
+
+
+
+        // TODO 5.将配置信息流处理成广播流
+
+
+        // TODO 6.链接主流与广播流
+
+
+        // TODO 7.广播流数据处理主流数据
+
+
+        // TODO 8.将数据写出到Phoenix中
+
+
+        // TODO 9.启动任务
+
+    }
+}
